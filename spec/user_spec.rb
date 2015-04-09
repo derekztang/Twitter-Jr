156ef1c9e4a774f0d92de53beab1e350ac8596e5
Campbell Belden & Ayako Kurushima <pair+Campbellb.miniyakkos@devbootcamp.com>
2015-04-09 11:37:17 -0700

    added databases and tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,6 +7,24 @@ describe User do
 		cam = User.create!
 		joe = User.create!
 		pete = User.create!
+
+    bob.followees << laura
+    bob.followees << cam
+    bob.followers << joe
+    bob.followers << pete
+    bob.save!
+
+    bob.reload
+
+    expect(bob.followees).to include laura
+    expect(bob.followees).to include cam
+    expect(bob.followers).to include joe
+    expect(bob.followers).to include pete
+
+    expect(laura.followers).to include bob
+    expect(cam.followers).to include bob
+    expect(joe.followees).to include bob
+    expect(pete.followees).to include bob
+
 	end
 end
-
